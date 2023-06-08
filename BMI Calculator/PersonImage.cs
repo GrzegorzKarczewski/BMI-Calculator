d776c146920658a7e6aadda92bf7fab99f893646
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-06-08 19:41:59 +0200

    Created PersonImage class

---
+++ BMI Calculator/PersonImage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+using System.Windows.Controls;
+
+namespace BMI_Calculator; 
+
+public class PersonImage {
+    // Move SetPersonImage() method from MainWindow.xaml.cs to this class
+    public static void SetPersonImage(WeightType weightType, int gender) {
+        Image image = new Image();
+        
+        if (gender == 0) {
+            string imagePath = weightType switch
+            {
+                WeightType.Low => "Images/male_skinny.png",
+                WeightType.High => "Images/male_fat.png",
+                WeightType.Normal => "Images/male_regular.png",
+                _ => String.Empty
+            };
+            image.Source = new BitmapImage(new Uri(imagePath, UriKind.Relative));   
+        }
+        else {
+            string imagePath = weightType switch
+            {
+                WeightType.Low => "Images/male_skinny.png",
+                WeightType.High => "Images/male_fat.png",
+                WeightType.Normal => "Images/male_regular.png",
+                _ => String.Empty
+            };
+            image.Source = new BitmapImage(new Uri(imagePath, UriKind.Relative));   
+        }
+    }
+}
