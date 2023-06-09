b047cd43b2cddf890ae100ed02488ff64f7954a9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2023-06-09 14:44:15 +0200

    Moved window-related class into "Window" folder

--- BMI Calculator/BMITips.cs
+++ BMI Calculator/BMITips.cs
@@ -8,10 +8,8 @@ namespace BMI_Calculator
 {
     public class BMITips
     {
-       
-            public List<string> underweight_tips { get; set; }
-            public List<string> healthy_weight_tips { get; set; }
-            public List<string> overweight_tips { get; set; }
-        
+        public List<string> underweight_tips { get; set; }
+        public List<string> healthy_weight_tips { get; set; }
+        public List<string> overweight_tips { get; set; }
     }
 }
